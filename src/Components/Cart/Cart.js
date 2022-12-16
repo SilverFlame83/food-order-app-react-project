54cdb98afada0503a4ba3f78ea94130c2c2e0f9a
SilverFlame83 <imren_83@abv.bg>
2022-12-16 21:10:45 +0200

    Changes on Cart function added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,27 +4,28 @@ import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 
-
-const cartCtx = useContext(CartContext);
-
-const cartItems = (
-  <ul className={styles["cart-items"]}>
-    {cartCtx.map((item) => (
-      <li>{item.name}</li>
-      ))}
-  </ul>
-);
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartCtx.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
   return (
     <Modal onCancel={props.onCancel}>
-        {cartItems}
+      {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>35.87</span>
       </div>
       <div className={styles.actions}>
-        <button onClick={props.onCancel} className={styles["button--alt"]}>Close</button>
+        <button onClick={props.onCancel} className={styles["button--alt"]}>
+          Close
+        </button>
         <button className={styles.button}>Order</button>
       </div>
     </Modal>
